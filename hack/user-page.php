5b12da00f18408a238b8f81dc54bda4b46c95e71
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2024-02-04 15:54:52 +0200

    Implemented dynamic resizing for the message text area and optimized message display

--- hack/user-page.php
+++ hack/user-page.php
@@ -32,8 +32,8 @@ if (isset($_GET['username'])) {
         <ul class="message-block" id="messagesContainer"></ul>
 
         <form>
-            <label for="">
-                <textarea class="message-textarea search-friend__input" name="" id="messageTextarea" placeholder="Write your message" cols="30" rows="10"></textarea>
+            <label for="messageTextarea">
+                <textarea class="message-textarea search-friend__input" id="messageTextarea" placeholder="Write your message" rows="1"></textarea>
             </label>
             <button class="message-button" type="button" onclick="sendMessages(<?php echo $userData['id']; ?>, event)">Send</button>
         </form>
@@ -45,6 +45,14 @@ if (isset($_GET['username'])) {
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             getCurrentUserSubscriptions(<?php echo $userData['id']; ?>);
+        });
+    </script>
+    <script>
+        const textarea = document.getElementById('messageTextarea');
+
+        textarea.addEventListener('input', function() {
+            this.style.height = 'auto'; // Автоматично збільшити висоту
+            this.style.height = (this.scrollHeight) + 'px'; // Збільшити висоту до фактичного розміру
         });
     </script>
     <!-- <script src="messages-js/messages.js"></script> -->
@@ -88,85 +96,9 @@ if (isset($_GET['username'])) {
     </script>
 
     <script>
-        const messagesContainer = document.getElementById('messagesContainer');
-
-        // async function loadMessages() {
-        //     try {
-        //         const response = await fetch('hack/messages/get_messages.php', {
-        //             method: 'GET',
-        //             headers: {
-        //                 'Content-Type': 'application/json',
-        //             },
-        //         });
-
-        //         if (response.ok) {
-        //             const messages = await response.json();
-
-        //             console.log('messages', messages);
-
-        //             const messagesHTML = messages.reverse().map(message => `
-        //                 <li class="message-li">
-        //                     <p>${message.message_text}</p>
-        //                     <button class="message-li__button" onclick="deleteMessage(${message.id}, event)">Delete</button>
-        //                 </li>
-        //             `).join('');
-
-        //             messagesContainer.innerHTML = messagesHTML;
-        //             // messagesContainer.insertAdjacentHTML('', messagesHTML);
-
-        //         } else {
-        //             console.error('Failed to fetch messages');
-        //         }
-        //     } catch (error) {
-        //         console.error('Error in fetch request', error);
-        //     }
-        // }
-
-        // loadMessages();
-
-        // async function loadMessages() {
-        //     try {
-        //         const response = await fetch('hack/messages/get_messages.php', {
-        //             method: 'GET',
-        //             headers: {
-        //                 'Content-Type': 'application/json',
-        //             },
-        //         });
-
-        //         if (response.ok) {
-        //             const messages = await response.json();
-
-        //             console.log('messages', messages);
-
-        //             // Вивести кожне повідомлення та інформацію про користувача
-        //             for (const message of messages) {
-        //                 const user = await getUserInfo(message.sender_id);
-        //                 console.log('User Info:', user);
-
-        //                 const messageHTML = `
-        //                     <li>
-        //                         <a class="message-a" href='index.php?page=user&username=${encodeURIComponent(user.name)}'>
-        //                             <div>
-        //                                 <img class="message-img" src='hack/${user.avatar}' alt='${user.name}'>
-        //                                 <p class="">${user.name}</p>
-        //                             </div>
-        //                             <p class="message-a__text">${message.message_text}</p>
-        //                             <button class="message-a__button" onclick="deleteMessage(${message.id}, event)">Delete</button>
-        //                         </a>
-        //                     </li>`;
-
-        //                 messagesContainer.insertAdjacentHTML('beforeend', messageHTML);
-        //             }
-        //         } else {
-        //             console.error('Failed to fetch messages');
-        //         }
-        //     } catch (error) {
-        //         console.error('Error in fetch request', error);
-        //     }
-        // }
-        // loadMessages();
-
         async function loadMessages() {
+            const messagesContainer = document.getElementById('messagesContainer');
+
             try {
                 const response = await fetch('hack/messages/get_messages.php', {
                     method: 'GET',
@@ -178,30 +110,32 @@ if (isset($_GET['username'])) {
                 if (response.ok) {
                     const messages = await response.json();
 
-                    console.log('messages', messages);
+                    const fragment = document.createDocumentFragment();
 
-                    // Очищення контейнера перед вставкою нових повідомлень
-                    messagesContainer.innerHTML = '';
-
-                    // Вивести кожне повідомлення та інформацію про користувача
-                    for (const message of messages) {
+                    for (const message of messages.reverse()) {
                         const user = await getUserInfo(message.sender_id);
-                        console.log('User Info:', user);
 
                         const messageHTML = `
-                    <li>
-                        <a class="message-a" href='index.php?page=user&username=${encodeURIComponent(user.name)}'>
-                            <div>
-                                <img class="message-img" src='hack/${user.avatar}' alt='${user.name}'>
-                                <p class="">${user.name}</p>
-                            </div>
-                            <p class="message-a__text">${message.message_text}</p>
-                            <button class="message-a__button" onclick="deleteMessage(${message.id}, event)">Delete</button>
-                        </a>
-                    </li>`;
+                            <li class="message-li">
+                                <a class="message-a" href='index.php?page=user&username=${encodeURIComponent(user.name)}'>
 
-                        messagesContainer.insertAdjacentHTML('beforeend', messageHTML);
+                                    <img class="message-img" src='hack/${user.avatar}' alt='${user.name}'> 
+
+                                    <div class="message-div">
+                                        <div class="message-blk">
+                                            <p class="message-name">${user.name}</p>
+                                            <p class="message-a__text">${message.message_text}</p>
+                                        </div>
+                                        <button class="message-a__button" onclick="deleteMessage(${message.id}, event)">Delete</button>
+                                    </div>
+                                </a>
+                            </li>`;
+
+                        fragment.appendChild(document.createRange().createContextualFragment(messageHTML));
                     }
+
+                    messagesContainer.innerHTML = '';
+                    messagesContainer.appendChild(fragment);
                 } else {
                     console.error('Failed to fetch messages');
                 }
@@ -209,16 +143,14 @@ if (isset($_GET['username'])) {
                 console.error('Error in fetch request', error);
             }
         }
-        loadMessages();
-
 
         async function getUserInfo(userId) {
+
             try {
                 const userResponse = await fetch(`hack/messages/get_user_by_id.php?id=${userId}`);
                 if (userResponse.ok) {
                     return await userResponse.json();
 
-                    console.log("userId", userId)
                 } else {
                     console.error('Failed to fetch user info');
                     return {};
@@ -228,10 +160,58 @@ if (isset($_GET['username'])) {
                 return {};
             }
         }
+
+        loadMessages();
+
+        // async function loadMessages() {
+        //     const messagesContainer = document.getElementById('messagesContainer');
+        //     try {
+        //         const response = await fetch('hack/messages/get_messages.php', {
+        //             method: 'GET',
+        //             headers: {
+        //                 'Content-Type': 'application/json',
+        //             },
+        //         });
+
+        //         if (response.ok) {
+        //             const messages = await response.json();
+
+        //             console.log('messages', messages);
+
+        //             // Очищення контейнера перед вставкою нових повідомлень
+        //             messagesContainer.innerHTML = '';
+
+        //             messages.map(async (message) => {
+        //                 const user = await getUserInfo(message.sender_id);
+
+        //                 const messageHTML = `
+        //             <li class="message-li">
+        //                 <a class="message-a" href='index.php?page=user&username=${encodeURIComponent(user.name)}'>
+
+        //                     <img class="message-img" src='hack/${user.avatar}' alt='${user.name}'> 
+
+        //                     <div class="message-div">
+        //                         <div class="message-blk">
+        //                             <p class="message-name">${user.name}</p>
+        //                             <p class="message-a__text">${message.message_text}</p>
+        //                         </div>
+        //                         <button class="message-a__button" onclick="deleteMessage(${message.id}, event)">Delete</button>
+        //                     </div>
+        //                 </a>
+        //             </li>`;
+
+        //                 messagesContainer.insertAdjacentHTML('beforeend', messageHTML);
+        //             });
+        //         } else {
+        //             console.error('Failed to fetch messages');
+        //         }
+        //     } catch (error) {
+        //         console.error('Error in fetch request', error);
+        //     }
+        // }
     </script>
 
     <script>
-        // Функція для видалення повідомлення
         async function deleteMessage(messageId, event) {
             event.preventDefault();
             try {
@@ -254,19 +234,6 @@ if (isset($_GET['username'])) {
             }
         }
     </script>
-
-
-
-
-
-    <!-- <li class="">
-        <a href='index.php?page=user&username=${encodeURIComponent(user.name)}'>
-            <img class="" src='hack/${user.avatar}' alt='${user.name}'>
-            <p class="">${user.name}</p>
-            <p>${message.message_text}</p>
-            <button onclick="deleteMessage(${message.id}, event)">Delete</button>
-        </a>
-    </li>`; -->
 
 </body>
 
