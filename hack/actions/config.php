ff4ea8d899f4b9674cf57331815c573a15c88fb7
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2024-02-24 12:01:33 +0200

    add

--- hack/actions/config.php
+++ hack/actions/config.php
@@ -1,25 +1,11 @@
 <?php
 
-const DB_HOST = 'localhost';
+const DB_HOST = 'mysql.zzz.com.ua';
 
 const DB_PORT = '3306';
 
-const DB_NAME = 'signup-signin';
+const DB_NAME = 'glowing_journey';
 
-const DB_USERNAME = 'root';
+const DB_USERNAME = 'GlowingJourney7';
 
-const DB_PASSWORD = '';
-
-
-
-
-
-// const DB_HOST = 'localhost';
-
-// const DB_PORT = '3306';
-
-// const DB_NAME = 'glowing_journey';
-
-// const DB_USERNAME = 'Petmel7';
-
-// const DB_PASSWORD = 'H86gurb54h%*)y';
+const DB_PASSWORD = '1234567Bd';
