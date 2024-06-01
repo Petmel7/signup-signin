70dc58a417de00f2f88a876dbca9ae13f0242e30
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2024-06-01 18:18:53 +0300

    add

--- js/sendMessages.js
+++ js/sendMessages.js
@@ -1,5 +1,4 @@
 
-// Підключення WebSocket до сервера
 const socket = new WebSocket(`ws://localhost:2346/?sender_id=${loggedInUserId}&recipient_id=${recipientId}`);
 
 socket.onopen = function () {
@@ -7,19 +6,27 @@ socket.onopen = function () {
 };
 
 socket.onmessage = async function (event) {
-    const message = JSON.parse(event.data);
-    console.log('socket.onmessage->message:', message);
-    console.log('socket.onmessage->message.messages', message.messages);
-    console.log('socket.onmessage->message.users', message.users);
+    const messagesData = JSON.parse(event.data);
+    console.log('onmessage->messagesData', messagesData);
 
-    try {
-        if (Array.isArray(message.messages) && message.messages.length !== 0) {
-            await displayMessages(message.messages, message.users);
-        } else {
-            console.log('Received message is not in expected format:', message);
+    if (messagesData.success) {
+        const messages = messagesData.success.messages;
+        const users = messagesData.success.users;
+
+        if (!Array.isArray(messages) || !Array.isArray(users)) {
+            console.error('Invalid messages or users format', { messages, users });
+            return;
         }
-    } catch (error) {
-        console.log('onmessageError', error);
+        await displayMessages(messages, users);
+    } else if (messagesData.delete) {
+        const messages = messagesData.delete.messages;
+        const users = messagesData.delete.users;
+
+        if (!Array.isArray(messages) || !Array.isArray(users)) {
+            console.error('Invalid messages or users format', { messages, users });
+            return;
+        }
+        await displayMessages(messages, users);
     }
 };
 
@@ -31,7 +38,6 @@ socket.onclose = function () {
     console.log('WebSocket connection closed');
 };
 
-// Функція для відправки повідомлень
 async function sendMessages(recipientId, event) {
     event.preventDefault();
     const messageTextarea = document.getElementById('messageTextarea');
@@ -54,52 +60,30 @@ async function sendMessages(recipientId, event) {
         alert('WebSocket connection is not open.');
     }
     messageTextarea.value = '';
-    await loadAndDisplayMessages(loggedInUserId, recipientId);
 }
 
-// Функція для завантаження та відображення повідомлень
-async function loadAndDisplayMessages(loggedInUserId, recipientId) {
-    const messagesContainer = document.getElementById('messagesContainer');
-
-    const response = await fetch('hack/messages/get_messages.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            sender_id: loggedInUserId,
-            recipient_id: recipientId
-        }),
-    });
-
-    const messagesData = await response.json();
-    console.log('loadAndDisplayMessages->messagesData', messagesData);
-
-    if (messagesData.success) {
-        const messages = messagesData.success.messages;
-        const users = messagesData.success.users;
-
-        if (!Array.isArray(messages) || !Array.isArray(users)) {
-            console.error('Invalid messages or users format');
-            return;
+async function deleteMessage(messageId, event) {
+    event.preventDefault();
+    try {
+        const deleteMessage = {
+            action: 'delete',
+            message_id: messageId
         }
 
-        console.log('displayMessages', messages);
+        socket.send(JSON.stringify(deleteMessage));
 
-        await displayMessages(messages, users);
-    } else {
-        console.error('Failed to load messages:', messagesData);
+    } catch (error) {
+        console.error('Error:', error);
     }
 }
 
-// Функція для відображення отриманих повідомлень
 async function displayMessages(messages, users) {
-    const messagesContainer = document.getElementById('messagesContainer');
-
     if (!Array.isArray(messages) || !Array.isArray(users)) {
-        console.error('Invalid messages or users format');
+        console.error('Invalid messages or users format', { messages, users });
         return;
     }
+
+    const messagesContainer = document.getElementById('messagesContainer');
 
     let lastSenderId = null;
     let lastMessageTime = null;
@@ -173,149 +157,3 @@ async function displayMessages(messages, users) {
     messagesContainer.innerHTML = messagesHTML;
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
-
-// Завантажити повідомлення при першому завантаженні сторінки
-loadAndDisplayMessages(loggedInUserId, recipientId);
-
-
-
-
-
-
-
-
-// // Підключення WebSocket до сервера
-// const socket = new WebSocket(`ws://localhost:2346/?sender_id=${loggedInUserId}&recipient_id=${recipientId}`);
-
-// socket.onopen = function () {
-//     console.log('WebSocket connection opened');
-// };
-
-// socket.onmessage = async function (event) {
-//     const messagesData = JSON.parse(event.data);
-//     console.log('=====:', messagesData);
-
-//     if (messagesData.success) {
-//         const messages = messagesData.success.messages;
-//         const users = messagesData.success.users;
-
-//         if (!Array.isArray(messages) || !Array.isArray(users)) {
-//             console.error('Invalid messages or users format');
-//             return;
-//         }
-//         await displayMessages(messages, users);
-//     };
-// }
-
-// socket.onerror = function (error) {
-//     console.error('WebSocket error:', error);
-// };
-
-// socket.onclose = function () {
-//     console.log('WebSocket connection closed');
-// };
-
-// // Функція для відправки повідомлень
-// async function sendMessages(recipientId, event) {
-//     event.preventDefault();
-//     const messageTextarea = document.getElementById('messageTextarea');
-//     const messageText = messageTextarea.value.trim();
-//     if (messageText === '') {
-//         alert('Please enter the text of the message.');
-//         return;
-//     }
-//     const message = {
-//         sender_id: loggedInUserId,
-//         recipient_id: parseInt(recipientId, 10),
-//         message_text: messageText
-//     };
-
-//     console.log('sendMessages->message', message);
-
-//     if (socket.readyState === WebSocket.OPEN) {
-//         socket.send(JSON.stringify(message));
-//     } else {
-//         alert('WebSocket connection is not open.');
-//     }
-//     messageTextarea.value = '';
-//     // displayMessages(messages, users)
-// }
-
-// // Функція для відображення отриманих повідомлень
-// async function displayMessages(messages, users) {
-//     const messagesContainer = document.getElementById('messagesContainer');
-
-//     let lastSenderId = null;
-//     let lastMessageTime = null;
-
-//     const messagesHTML = messages.map(message => {
-//         const senderId = message.sender_id;
-//         const isSender = senderId === loggedInUserId;
-//         const formattedTime = formatTime(message.sent_at);
-
-//         const { messageClass, displayStyle } = getMessageStyles(isSender);
-
-//         const currentTime = new Date(message.timestamp).getTime();
-//         const showAvatar = senderId !== lastSenderId || (currentTime - lastMessageTime > 60000);
-
-//         const { avatarDisplayStyle, marginLeftStyle, dynamicBorderStyle } = calculateStyles(showAvatar, isSender, displayStyle);
-
-//         const sender = users.find(user => user.id === senderId);
-
-//         if (!sender) {
-//             console.error('Sender not found:', senderId);
-//             return '';
-//         }
-
-//         if (showAvatar) {
-//             lastSenderId = senderId;
-//             lastMessageTime = currentTime;
-//         }
-
-//         const messageSentAtClass = message.message_text && message.message_text.length < 15 ? 'message-heder--sent_at' : 'message-header';
-
-//         const {
-//             backgroundSenderClass,
-//             backgroundClassMessages,
-//             recipientWhiteText,
-//             messageDateStyleDisplay,
-//             modalThemeStyle,
-//             mesageButtonStyle
-//         } = calculateStylesLocalStorage(isSender);
-
-//         const {
-//             encodedUsername,
-//             messageContent,
-//             backgroundImage,
-//             backgroundImageSize,
-//             backgroundSizeCover,
-//             imageButtonStyle,
-//             avatarSrc
-//         } = processMessageData(sender, message, recipientWhiteText);
-
-//         return `
-//         <li class="${messageClass}">
-//             <div class="messages">
-//                 <a href='index.php?page=user&username=${encodedUsername}'>
-//                     <img style="display: ${avatarDisplayStyle}" id="messageImg" class="message-img" src='${avatarSrc}' alt='${sender.name}'>
-//                 </a>
-//                 <div class="search-friend--add message-body ${backgroundSenderClass} ${backgroundClassMessages}" style="margin-left: ${marginLeftStyle}; border-radius: ${dynamicBorderStyle}; ${backgroundImage}; ${backgroundImageSize}; ${backgroundSizeCover}">
-
-//                     <div class="${messageSentAtClass}">
-//                         ${messageContent}
-//                         <span class="${messageDateStyleDisplay} ${imageButtonStyle}">${formattedTime}</span>
-//                     </div>
-//                     <button class="message-delete--button delete-button ${mesageButtonStyle} ${imageButtonStyle}" onclick="openModalDelete(${message.id}, ${isSender})">&#8942;</button>
-//                     <div id="myModal" class="modal">
-//                         <div class="modal-content ${modalThemeStyle}" id="modalContent"></div>
-//                     </div>
-
-//                 </div>
-//             </div>
-//         </li>`;
-//     }).join('');
-//     messagesContainer.innerHTML = messagesHTML;
-//     messagesContainer.scrollTop = messagesContainer.scrollHeight;
-// }
-
-// // displayMessages(messages, users);
--- webSocketServer/webSocketServer.php
+++ webSocketServer/webSocketServer.php
@@ -24,6 +24,17 @@ $ws_worker->onConnect = function ($connection) {
 
             echo "User $senderId connected as sender\n";
             echo "User $recipientId connected as recipient\n";
+
+            // Відправка історії повідомлень
+            $messages = getMessagesByRecipient($senderId, $recipientId);
+            $users = getAllUsers();
+
+            $responseData = [
+                'messages' => $messages,
+                'users' => $users
+            ];
+
+            $connection->send(json_encode(['success' => $responseData]));
         } else {
             echo "No sender or recipient ID provided\n";
         }
@@ -33,7 +44,39 @@ $ws_worker->onConnect = function ($connection) {
 $ws_worker->onMessage = function ($connection, $data) use (&$connectedUsers, &$connectionUserMap) {
     $message = json_decode($data, true);
 
-    if (isset($message['sender_id'], $message['recipient_id'], $message['message_text'])) {
+    if (isset($message['action']) && $message['action'] === 'delete') {
+        if (isset($message['message_id'])) {
+            $messageId = $message['message_id'];
+
+            try {
+                $conn = getPDO();
+
+                $sql = "DELETE FROM `messages` WHERE id = :message_id";
+                $stmt = $conn->prepare($sql);
+                $stmt->bindParam(':message_id', $messageId, PDO::PARAM_INT);
+                $stmt->execute();
+
+                foreach ($connectedUsers as $userConnection) {
+                    list($senderId, $recipientId) = $connectionUserMap[$userConnection->id];
+                    $messages = getMessagesByRecipient($senderId, $recipientId);
+                    $users = getAllUsers();
+
+                    $responseData = [
+                        'messages' => $messages,
+                        'users' => $users
+                    ];
+
+                    $userConnection->send(json_encode(['delete' => $responseData]));
+                }
+            } catch (PDOException $e) {
+                echo "Error deleting message: " . $e->getMessage();
+            } finally {
+                if ($conn !== null) {
+                    $conn = null;
+                }
+            }
+        }
+    } elseif (isset($message['sender_id'], $message['recipient_id'], $message['message_text'])) {
         $senderId = $message['sender_id'];
         $recipientId = $message['recipient_id'];
         $messageText = $message['message_text'];
@@ -41,21 +84,16 @@ $ws_worker->onMessage = function ($connection, $data) use (&$connectedUsers, &$c
         saveMessage($senderId, $recipientId, $messageText);
 
         foreach ($connectedUsers as $userConnection) {
-            $userConnection->send(json_encode(['messages' => [$message]]));
+            $messages = getMessagesByRecipient($senderId, $recipientId);
+            $users = getAllUsers();
+
+            $responseData = [
+                'messages' => $messages,
+                'users' => $users
+            ];
+
+            $userConnection->send(json_encode(['success' => $responseData]));
         }
-    } else {
-        $senderId = $message['sender_id'];
-        $recipientId = $message['recipient_id'];
-
-        $messages = getMessagesByRecipient($senderId, $recipientId);
-        $users = getAllUsers();
-
-        $responseData = [
-            'messages' => $messages,
-            'users' => $users
-        ];
-
-        $connection->send(json_encode(['success' => $responseData]));
     }
 };
 
@@ -73,79 +111,3 @@ $ws_worker->onClose = function ($connection) use (&$connectedUsers, &$connection
 };
 
 Worker::runAll();
-
-
-
-
-
-
-// require_once __DIR__ . '../../vendor/autoload.php';
-// require_once __DIR__ . '../../hack/actions/helpers.php';
-
-// use Workerman\Worker;
-
-// $ws_worker = new Worker('websocket://0.0.0.0:2346');
-
-// $connectedUsers = [];
-// $connectionUserMap = [];
-
-// $ws_worker->onConnect = function ($connection) {
-//     echo "Connection open\n";
-//     $connection->onWebSocketConnect = function ($connection) {
-//         global $connectedUsers, $connectionUserMap;
-
-//         if (isset($_GET['sender_id']) && isset($_GET['recipient_id'])) {
-//             $senderId = $_GET['sender_id'];
-//             $recipientId = $_GET['recipient_id'];
-
-//             $connectedUsers[$connection->id] = $connection;
-//             $connectionUserMap[$connection->id] = [$senderId, $recipientId];
-
-//             echo "User $senderId connected as sender\n";
-//             echo "User $recipientId connected as recipient\n";
-//         } else {
-//             echo "No sender or recipient ID provided\n";
-//         }
-//     };
-// };
-
-// $ws_worker->onMessage = function ($connection, $data) use (&$connectedUsers, &$connectionUserMap) {
-//     $message = json_decode($data, true);
-
-//     if (isset($message['sender_id'], $message['recipient_id'], $message['message_text'])) {
-//         $senderId = $message['sender_id'];
-//         $recipientId = $message['recipient_id'];
-//         $messageText = $message['message_text'];
-
-//         saveMessage($senderId, $recipientId, $messageText);
-
-//         foreach ($connectedUsers as $userConnection) {
-//             // $userConnection->send(json_encode(['messages' => [$message]]));
-
-//             $messages = getMessagesByRecipient($senderId, $recipientId);
-//             $users = getAllUsers();
-
-//             $responseData = [
-//                 'messages' => $messages,
-//                 'users' => $users
-//             ];
-//             var_dump($responseData);
-//             $connection->send(json_encode(['success' => $responseData]));
-//         }
-//     }
-// };
-
-// $ws_worker->onClose = function ($connection) use (&$connectedUsers, &$connectionUserMap) {
-//     if (isset($connectionUserMap[$connection->id])) {
-//         list($senderId, $recipientId) = $connectionUserMap[$connection->id];
-
-//         unset($connectedUsers[$connection->id]);
-//         unset($connectionUserMap[$connection->id]);
-
-//         echo "Connection closed for sender $senderId and recipient $recipientId\n";
-//     } else {
-//         echo "Connection closed\n";
-//     }
-// };
-
-// Worker::runAll();
